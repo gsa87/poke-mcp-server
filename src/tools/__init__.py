978459d1fd8d3631d131aec4cdafcd384f393ce7
George Ankomah <george.ankomah@Georges-MacBook-Air.local>
2025-12-09 00:08:43 +0100

    fixed init

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -1,4 +1,2 @@
-from .weather import register_weather
-from .ns import register_ns
-
-__all__ = ["register_weather", "register_ns"]
+from .weather import register as register_weather
+from .ns import register as register_ns
